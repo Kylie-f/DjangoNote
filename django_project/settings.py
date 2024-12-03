3701bf32e08f47ddc2275f28cc121e6c201d8074
Sydney Escobar <se176171@umconnect.umt.edu>
2024-12-03 11:26:56 -0700

    WIP login/signup page, buttons, and forms

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,6 +127,11 @@ STATIC_URL = '/static/css/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = "accounts.CustomUser"
+
+LOGIN_REDIRECT_URL = "notes" 
+LOGOUT_REDIRECT_URL = "home"
+
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
--- django_project/urls.py
+++ django_project/urls.py
@@ -17,12 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from django.conf import settings
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="home.html"), name="home"),
+    path("accounts/", include("accounts.urls")), 
+    path("accounts/", include("django.contrib.auth.urls")),
     path("notes/", include("notes.urls")),
 ] + debug_toolbar_urls()
 
