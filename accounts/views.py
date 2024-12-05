4f8a4f0d65865f2cc83f2bae9496008b463ec32b
Sydney Escobar <se176171@umconnect.umt.edu>
2024-12-05 10:17:48 -0700

    can signup, login, and logout

--- accounts/views.py
+++ accounts/views.py
@@ -7,3 +7,13 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+    
+    def form_valid(self, form):
+        # Print any form validation issues
+        print("Form is valid")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        # Print errors if the form is invalid
+        print("Form errors:", form.errors)
+        return super().form_invalid(form)
